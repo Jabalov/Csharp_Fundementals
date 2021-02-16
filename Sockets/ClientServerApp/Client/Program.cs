3c03232e431ae9e48e3c29ab0ceb1eccd788401f
jabalov <moh.aboajabl@gmail.com>
2021-02-16 13:34:37 +0200

    low Level Chat app

---
+++ Sockets/ClientServerApp/Client/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ip = null;
+
+            try
+            {
+                Console.WriteLine("Socket Client Starte:");
+                Console.WriteLine("Write a valid ip address and press enter!");
+                string strIPAddress = Console.ReadLine();
+
+                Console.WriteLine("Apply a valid port number:");
+                string strPort = Console.ReadLine();
+                int nPort = 0;
+
+                IPAddress ipaddr;
+                if (!IPAddress.TryParse(strIPAddress, out ipaddr))
+                {
+                    Console.WriteLine("Invalid Server IP");
+                    return;
+                }
+                if (!int.TryParse(strPort, out nPort))
+                {
+                    Console.WriteLine("Invalid Server Port");
+                    return;
+                }
+
+                if (nPort <= 0 || nPort > 65535)
+                {
+                    Console.WriteLine("Enter a Valid Port Number");
+                }
+
+                Console.WriteLine($"IPAddress: {ipaddr.ToString()}, PortNumber: {nPort}");
+                client.Connect(ipaddr, nPort);
+
+                string inputCommand = string.Empty;
+
+                while(true)
+                {
+                    inputCommand = Console.ReadLine();
+                    if (inputCommand == "exit")
+                        break;
+
+                    var bufferSend  = Encoding.ASCII.GetBytes(inputCommand);
+                    client.Send(bufferSend);
+                    
+                    var bufferRecieved = new byte[128];
+                    int nRec = client.Receive(bufferRecieved);
+
+                    Console.WriteLine($"{ Encoding.ASCII.GetString(bufferRecieved, 0, nRec)}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                if(client != null)
+                {
+                    if(client.Connected)
+                        client.Shutdown(SocketShutdown.Both);
+
+                    client.Close();
+                    client.Dispose();
+                }
+                
+            }
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sockets/ClientServerApp/Server/Program.cs
@@ -0,0 +1,54 @@
+﻿ using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Loopback;
+            IPEndPoint endPoint = new IPEndPoint(ip, 23000);
+
+            try
+            {
+                socketListener.Bind(endPoint);
+                socketListener.Listen(5);
+
+                Socket client = socketListener.Accept();
+                Console.WriteLine($"Client Connected {client.ToString()} - IP End-Point {client.RemoteEndPoint.ToString()}");
+
+                byte[] buffer = new byte[128];
+                int numOfRecievedBytes = 0;
+
+                while (true)
+                {
+                    numOfRecievedBytes = client.Receive(buffer);
+                    Console.WriteLine($"Number Of recieved bytes: {numOfRecievedBytes}");
+
+                    var recieved = Encoding.ASCII.GetString(buffer, 0, numOfRecievedBytes);
+                    Console.WriteLine($"Data is: {recieved}");
+
+                    client.Send(buffer);
+
+                    if (recieved == "x")
+                        break;
+
+                    Array.Clear(buffer, 0, buffer.Length);
+                    numOfRecievedBytes = 0;
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
+        }
+
+    }
+}
